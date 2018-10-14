f6b307cbd59aa85e933f949036abda6303fcc6ac
Vali Sumurduc <vali_sum@outlook.com>
2018-10-14 15:32:17 +0200

    Step 3 v2

--- steps/step3/functionality.js
+++ steps/step3/functionality.js
@@ -14,6 +14,22 @@ var dy = -2;
 
 var color = '#0095DD';
 
+function getColor() {
+	colors = [
+		'#ff0000',
+		'#33cc33',
+		'#ffff00',
+		'#0000ff',
+		'#66ffff',
+		'#800000',
+		'#ff9900',
+	];
+
+	let color = colors[Math.floor(Math.random()* colors.length)];
+
+	return color;
+}
+
 function drawBall(color) {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -29,12 +45,14 @@ function draw() {
 	// Colisiones
 	if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
 		dx = -dx;
-		color = 'red';
+		color = getColor();
 	}
+
+
 
 	if (y + dy > canvas.height -ballRadius || y + dy < ballRadius) {
 		dy = -dy;
-		color = 'green';
+		color = getColor();
 	}
 
 	x += dx;
